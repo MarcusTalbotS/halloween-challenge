a6640be6b0434c06e8287839dfc87213d8a172b3
MarcusTalbotS <1041464>
2022-10-21 20:22:53 +0200

    Added local class in MainEnum-constructor, to serve as specific Exception for the try-catch-finally block.

--- Java/src/main/java/nl/marcustalbot/Main.java
+++ Java/src/main/java/nl/marcustalbot/Main.java
@@ -1,8 +1,14 @@
 package nl.marcustalbot;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("""
+        logger.log(Level.INFO, """
         ====================================================================================================================================================
         | Welcome to the repository containing scarily bad code!                                                                                           |
         | =================================================================================================================================================|
--- Java/src/main/java/nl/marcustalbot/enums/MainEnum.java
+++ Java/src/main/java/nl/marcustalbot/enums/MainEnum.java
@@ -1,5 +1,8 @@
 package nl.marcustalbot.enums;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * While it is already pretty interesting that a main-method can legally occur in an enum, the real kicker is that the
  * constructor of an enum is executed before the main-method. This allows the user to run an entire program, before the
@@ -10,17 +13,24 @@ public enum MainEnum {
     MAIN;
 
     MainEnum() {
+
+        class HelloWorldException extends RuntimeException {
+            public HelloWorldException(final String message) {
+                super(message);
+            }
+        }
+
         try {
-            throw new RuntimeException("Hello, World!");
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
+            throw new HelloWorldException("Hello, World!");
+        } catch (HelloWorldException e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, e.getMessage());
         } finally {
             System.exit(0);
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("What is my purpose?");
+        Logger.getGlobal().log(Level.INFO, "What is my purpose?");
     }
 
 }
