88fd733542b874366a6ae8489aaea9802071fb95
MarcusTalbotS <1041464>
2022-10-21 20:02:14 +0200

    Added a concise description of the goal of this repository to Main.java.

--- src/main/java/nl/marcustalbot/Main.java
+++ src/main/java/nl/marcustalbot/Main.java
@@ -2,6 +2,14 @@ package nl.marcustalbot;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("""
+        ====================================================================================================================================================
+        | Welcome to the repository containing scarily bad code!                                                                                           |
+        | =================================================================================================================================================|
+        | The aim of this repository is to have fun exploring bad/unconventional features, and to exploit regular features to produce questionable code.   |
+        | This repository does not aim to be a repository of low-quality/broken code.                                                                      |
+        | All code posted to this repository must be functional, and interesting in some way.                                                              |
+        ====================================================================================================================================================
+        """);
     }
 }
